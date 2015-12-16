928bf4c8f8dcc8e94bd054ce535dd72271d050c5
NPSDC <noorpratap@gmail.com>
2015-12-16 13:41:10 +0530

    Added good resources to important links about MCA/CA and updated mca_cancer with tutorial

--- Extract/mca_all_cancer/mca_cancer.R
+++ Extract/mca_all_cancer/mca_cancer.R
@@ -101,3 +101,51 @@ for(i in seq(length(unique.cancer.genes)))
   }
   cancer.all.gene.wise[i,] = vals
 }
+
+
+###Starting with the mca analysis
+data.cancer.gene = t(cancer.all.gene.wise)
+data.cancer.gene = data.frame(data.cancer.gene)
+View(data.cancer.gene)
+library(FactoMineR)
+mca.data.cancer <- MCA(data.cancer.gene, ind.sup = 1)
+
+#Learning MCA (Refer important links)
+#library(FactoMineR)
+#library(factoextra)
+
+########fviz_screeplot(res.mca) finds suitable dimensions
+# Our data contains 13 rows and 4 columns.
+#If the data were random, the expected value of the eigenvalue for each
+#axis would be 1/(nrow(housetasks)-1) = 1/12 = 8.33% in terms of rows.
+#Likewise, the average axis should account for 1/(ncol(housetasks)-1) = 1/3 = 33.33% 
+#in terms of the 4 columns
+
+######plot(res.mca) 
+#######fviz_mca_biplot(res.mca) better representation of the above 
+#######fviz_mca_biplot(res.mca) + theme_minimal() even better version
+#Note in the above you can always arguments such as axes = c(1,2), col.row = "blue", col.col = "red"
+#Above is a symmetric plot in which distance b/w row/row or column/column can be interpreted but not
+#b/w column and row 
+ 
+####fviz_mca_var(res.mca) plots only variables(columns) 
+# library("corrplot")
+######corrplot(var$contrib, is.corr = FALSE) plots the contributions of variables along different dimensions
+######fviz_contrib(res.mca, choice = "var", axes = 1) contribution of variables along dimension 1 
+#fviz_contrib(res.mca, choice = "var", axes = 1:2) total contribution
+#fviz_contrib(res.mca, choice = "var", axes = 1, top = 10) top 10 variables
+
+#####fviz_mca_var(res.mca, col.var="contrib")+
+#####scale_color_gradient2(low="white", mid="blue", 
+#                      high="red", midpoint=2)+theme_minimal() 
+# plots the variables but also shows which variable has larger contribution
+# 
+# 
+# 
+# Individuals
+#fviz_contrib(res.mca, choice = "ind", axes = 1:2, top = 20) contribution individual wise
+#fviz_mca_ind(res.mca, col.ind="contrib")+
+#scale_color_gradient2(low="white", mid="blue", 
+#                      high="red", midpoint=0.85)+theme_minimal()
+#fviz_mca_biplot(res.mca, map ="rowprincipal", arrow = c(TRUE, TRUE))
+
